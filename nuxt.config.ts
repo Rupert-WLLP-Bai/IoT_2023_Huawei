e73da36d4ed33ffa9945392f6794bf25249c371f
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-07-23 17:56:45 +0800

    设置了网站的title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,13 @@ export default defineNuxtConfig({
   css: [
     '@/assets/css/main.css',
   ],
+  app:{
+    head:{
+      title: '石油电缆防偷盗监测系统',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+      ],
+    },
+  }
 })
